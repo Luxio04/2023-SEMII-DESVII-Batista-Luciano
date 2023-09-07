996ac5f78ea9ab26ce999fd624aa8cefa14e3517
Luciano Batista <lucianobatista_0499@hotmail.com>
2023-09-06 20:36:05 -0500

    laboratorio 1

---
+++ laboratorio 1/lab1.7.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+ <title>laboratorio 1.7 </title>
+</head>
+<body>
+<?php
+	$x = 24;
+	$pi = 3.1416;
+	$animal = "conejo";
+	$saludo = "hola caracola";
+	echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo;
+?>
+</body>
+</html>
