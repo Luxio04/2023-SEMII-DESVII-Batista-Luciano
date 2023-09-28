68236175209d34533c18d8e472c6c3a9af412192
Luciano Batista <lucianobatista_0499@hotmail.com>
2023-09-27 20:00:13 -0500

    subiendo laboratorio 5

--- laboratorio 5/lab52.php
+++ laboratorio 5/lab52.php
@@ -1,22 +1,30 @@
 <?php
-if (is_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name']))
-{
- $nombreDirectorio = "c:\XAMPP\htdocs";
- $nombrearchivo = $_FILES['nombre_archivo_cliente']['name'];
- $nombreCompleto = $nombreDirectorio . $nombrearchivo;
- if (is_file($nombreCompleto))
- {
- $idUnico = time();
- $nombrearchivo = $idUnico . "-" . $nombrearchivo;
-echo "Archivo repetido, se cambiara el nombre a $nombrearchivo 
-<br><br>";
- }
- move_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'],
- $nombreDirectorio . $nombrearchivo);
- 
- echo "El archivo se ha subido satisfactoriamente al directorio 
-$nombreDirectorio <br>";
+if (isset($_FILES['miArchivo']) && $_FILES['miArchivo']['error'] === 0) {
+    $tamanoMaximo = 2 * 1024 * 1024; 
+    $tiposPermitidos = ['image/jpeg', 'image/png'];
+
+    $nombreDirectorio = "archivos/";
+    $nombrearchivo = $_FILES['miArchivo']['name'];
+    $nombreCompleto = $nombreDirectorio . $nombrearchivo;
+
+    
+    if (file_exists($nombreCompleto)) {
+        $idUnico = time();
+        $nombrearchivo = $idUnico . "-" . $nombrearchivo;
+        echo "Archivo repetido, se cambiará el nombre a $nombrearchivo <br><br>";
+    }
+
+   
+    if ($_FILES['miArchivo']['size'] <= $tamanoMaximo && in_array($_FILES['miArchivo']['type'], $tiposPermitidos)) {
+        if (move_uploaded_file($_FILES['miArchivo']['tmp_name'], $nombreDirectorio . $nombrearchivo)) {
+            echo "El archivo se ha subido satisfactoriamente al directorio $nombreDirectorio <br>";
+        } else {
+            echo "No se ha podido subir el archivo al servidor <br>";
+        }
+    } else {
+        echo "El archivo no cumple con los requisitos de tamaño o tipo permitido <br>";
+    }
+} else {
+    echo "No se ha podido subir el archivo <br>";
 }
-else
- echo "No se ha podido subir el archivo <br>";
 ?>
