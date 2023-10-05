916cf1fb5df8fbc757117d1f8af395f399dd357b
Luciano Batista <lucianobatista_0499@hotmail.com>
2023-10-05 18:42:58 -0500

    subiendo laboratorio 7

---
+++ laboratorio 7/Lab76.php
@@ -0,0 +1,11 @@
+<?php
+include('class_lib.php');
+$altu=0;
+$diam = $_POST['diam'];
+$altu = $_POST['altu'];
+$cil = new Cilindro($diam,$altu);
+$volumen=$cil->calc_volumen();
+$area=$cil->calc_area();
+echo "<br/> El volumen del cilindro es de ". $volumen . " metros cubicos";
+echo "<br/> El area del cilindro es de ". $area . " metros cuadrados";
+?>
